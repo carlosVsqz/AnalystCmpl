3712c51cb5ae8e114d5ac6b4e9b9c024e301fb58
Carlos Vasquez <carls10vasquez@gmail.com>
2021-03-22 01:10:54 -0600

    adding Resource bundle to support lang es and us, adding support to log4j

--- src/main/java/com/escod/compiler/MainApp.java
+++ src/main/java/com/escod/compiler/MainApp.java
@@ -6,18 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/scene.fxml"));
-
-        Scene scene = new Scene(root);
+        ResourceBundle resources = ResourceBundle.getBundle("values", Locale.getDefault());
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/scene.fxml"), resources);
+        Scene scene = new Scene(root, 500, 300);
         scene.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm());
-
         stage.setTitle("JavaFX and Gradle");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- src/main/java/com/escod/compiler/controllers/FXMLController.java
+++ src/main/java/com/escod/compiler/controllers/FXMLController.java
@@ -1,21 +1,43 @@
 package com.escod.compiler.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 public class FXMLController implements Initializable {
 
-    @FXML
-    private Label label;
+    public Label lblInfo;
+    public Button btnAction;
+    public Label lblResult;
+    public Label lblHead;
+    private ResourceBundle resourceBundle;
+
+    Logger logger = LoggerFactory.getLogger(FXMLController.class);
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        this.resourceBundle = rb;
+        btnAction.setOnAction(this::getResult);
+    }
+
+    private void getResult(ActionEvent event) {
+        boolean isValid = false;
+
+        if (event != null)
+            event.consume();
+
+        if (isValid) {
+            logger.info("valid: " + isValid);
+            lblResult.setText(resourceBundle.getString("text_valid_string_result"));
+        } else {
+            logger.info("valid: " + isValid);
+            lblResult.setText(resourceBundle.getString("text_invalid_string_resut"));
+        }
     }
 }
